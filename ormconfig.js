8c6c6091a09595e34758d9a1173b75bf04275990
gnf22 <gnferreira2000@gmail.com>
2021-01-15 12:30:22 -0300

    Adjusting ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -10,10 +10,10 @@ module.exports = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
   entities: [
-    "./dist/modules/**/infra/typeorm/entities/*.js"
+    "./src/modules/**/infra/typeorm/entities/*.ts"
   ],
   migrations: [
-    "./dist/shared/infra/typeorm/migrations/*.js"
+    "./src/shared/infra/typeorm/migrations/*.ts"
   ],
   cli: {
     "migrationsDir": "./src/shared/infra/typeorm/migrations"
